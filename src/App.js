6bd687f40c08cb4afca89d3f8fe86ab9a18b0ac4
Grozoff <grozoff92@gmail.com>
2022-08-08 14:26:30 +0700

    lesso two

--- src/App.js
+++ src/App.js
@@ -1,16 +1,53 @@
-import React from 'react';
-import Message from './Message';
+import React, { useEffect, useState } from 'react';
+import './Message.css';
 
-const myMessage = 'Hello World!';
+const App = () => {
+  const [messageList, setMessageList] = useState([{ text: 'text', author: 'author' }]);
+  const [value, setValue] = useState('');
 
-class App extends React.Component {
-  render() {
-    return (
-      <div >
-        <Message message={myMessage} />
-      </div>
+  useEffect(() => {
+    if (messageList[messageList.length - 1].author === "user") {
+      setTimeout(() => {
+        setMessageList(
+          [...messageList, { text: 'autoreply', author: 'robot' }]
+        )
+      }, 1500)
+    }
+  }, [messageList])
+
+  const addMessage = () => {
+    if (value) {
+      setMessageList(
+        [...messageList, { text: value, author: "user" }]
+      )
+    }
+
+  };
+
+  const removeMessages = () => {
+    setMessageList(
+      messageList.filter((item) => item.author === 'author')
     )
-  }
+  };
+
+  return (
+    <div className="Message">
+      <header className="Message-header">
+        Messages:
+        {messageList.map((message) =>
+          <div>
+            <h4>Message: {message.text}</h4>
+            <h5>Author: {message.author}</h5>
+            <hr />
+          </div>)}
+        <button onClick={addMessage}>Add message</button>
+        <button onClick={removeMessages}>Remove all messages</button>
+        <input value={value} onChange={e => setValue(e.target.value)}></input>
+      </header>
+    </div>
+  )
 }
+
+
 
 export default App;
