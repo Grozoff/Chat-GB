1ed10301f78d5d3beb210e9c14ce28c4a19cf655
Sergei Grozov <grozov.sv@gmail.com>
2022-09-04 00:39:04 +0500

    lesson6

--- src/components/chat-list/chat-list.js
+++ src/components/chat-list/chat-list.js
@@ -1,20 +1,51 @@
-import { useState } from "react";
 import { Chat } from "./chat";
 import List from "@mui/material/List";
-import { NavLink, useParams } from "react-router-dom";
+import { NavLink, useParams, useNavigate } from "react-router-dom";
+import { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  deleteConversation,
+  createConversation,
+  conversationsSelector,
+} from "../../store/conversations";
 
 export const ChatList = () => {
-  const [chatList] = useState(["room1", "room2", "room3"]);
+  const conversations = useSelector(conversationsSelector);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const { roomId } = useParams();
+
+  const deleteConversationByName = useCallback(
+    (name, e) => {
+      e.preventDefault();
+      dispatch(deleteConversation(name));
+      navigate("/chat");
+    },
+    [dispatch, navigate]
+  );
+
+  const createConversationByName = () => {
+    const name = prompt("Enter the name: ");
+    const isValidName = !conversations.includes(name);
+
+    if (name && isValidName) {
+      dispatch(createConversation(name));
+    } else {
+      alert("Not a valid name");
+    }
+  };
+
   return (
     <List component="nav">
-      {chatList.map((chat) => (
-        <NavLink
-          style={{ color: "inherit", textDecoration: "inherit" }}
-          key={chat}
-          to={`/chat/${chat}`}
-        >
-          <Chat title={chat} selected={chat === roomId} />
+      <button onClick={createConversationByName}>create</button>
+
+      {conversations.map((chat) => (
+        <NavLink key={chat} to={`/chat/${chat}`}>
+          <Chat
+            title={chat}
+            selected={chat === roomId}
+            deleteConversationByName={deleteConversationByName}
+          />
         </NavLink>
       ))}
     </List>
